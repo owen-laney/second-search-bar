b08e513f6cfed1bf36fa7e2666513d1f67af7ebb
Owen <owenlaney@gmail.com>
2022-04-27 15:33:03 -0400

    fix:prevent null input

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,10 +1,19 @@
+//Focuses search bar for faster searching
+window.onload = function () {
+    document.getElementById("searchBar").focus();
+};
+
 document.getElementById("search-btn").onclick = function() {search()};
+
 
 function search() {
     var searchQuery = document.getElementById("searchBar").value;
-    searchQuery.replace(/\s/g, '+');
-    browser.search.search({
-        query: searchQuery,
-        engine: "GoogleScholar"
-    });
+    //Rejects null inputs & replaces spaces with '+'
+    if (searchQuery) {
+        searchQuery.replace(/\s/g, '+');
+        browser.search.search({
+            query: searchQuery,
+            engine: "GoogleScholar"
+        });
+    }
 }
